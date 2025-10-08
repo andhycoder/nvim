a5e9b931c7e600dc101d2e698c215b5238bbe2fd
AndhyAmar <amarzoni050@gmail.com>
2025-10-08 20:13:30 +0800

    inilitization configs

---
+++ lua/config/options.lua
@@ -0,0 +1,11 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+local o = vim.o
+
+o.tabstop = 2
+o.softtabstop = 2
+o.shiftwidth = 2
+
+o.swapfile = false
